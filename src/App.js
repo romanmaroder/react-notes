32e26a8bd28251ebace0a01db99b3eb4a784834c
RomanKashik <kashik.roman@rambler.ru>
2021-03-11 18:54:27 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import RegistrForm from './components/RegistrForm/RegistrForm'
+import React from "react";
+
+const App = () => {
+    return (
+        <div className="App">
+            <RegistrForm />
+        </div>
+    );
+}
+
+export default App;
